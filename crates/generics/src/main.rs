e314e3174224b2b614000107c4a879aea3894663
alessio.marchi <alessio.marchi@accenture.com>
2022-07-18 00:39:17 +0200

    feat: add generics using where

--- crates/generics/src/main.rs
+++ crates/generics/src/main.rs
@@ -20,8 +20,15 @@ fn move_entity<T: Movable>(entity: &T, x: i32, y: i32) {
   entity.move_by(x, y);
 }
 
+fn move_entity_where<T>(entity: &T, x: i32, y: i32)
+  where
+    T: Movable
+{
+  entity.move_by(x, y);
+}
+
 fn main() {
   let player = Player { name: "Player".to_owned(), x: 0, y: 0 };
   move_entity(&player, 1, 2);
-  move_entity(&player, 1, 2);
+  move_entity_where(&player, 2, 1);
 }
